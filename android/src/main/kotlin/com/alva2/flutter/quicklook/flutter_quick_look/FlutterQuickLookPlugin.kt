d2d28686616fd6a53147caf1e8f7c183e67c03bc
Jagmit Singh <js@skalio.com>
2021-05-03 19:00:49 +0200

    Regenerated android/ and example/android directory with lates flutter beta version

--- android/src/main/kotlin/com/alva2/flutter/quicklook/flutter_quick_look/FlutterQuickLookPlugin.kt
+++ android/src/main/kotlin/com/alva2/flutter/quicklook/flutter_quick_look/FlutterQuickLookPlugin.kt
@@ -1,21 +1,35 @@
 package com.alva2.flutter.quicklook.flutter_quick_look
 
+import androidx.annotation.NonNull
+
+import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
-class FlutterQuickLookPlugin: MethodCallHandler {
-  companion object {
-    @JvmStatic
-    fun registerWith(registrar: Registrar) {
-      val channel = MethodChannel(registrar.messenger(), "flutter_quick_look")
-      channel.setMethodCallHandler(FlutterQuickLookPlugin())
+/** FlutterQuickLookPlugin */
+class FlutterQuickLookPlugin: FlutterPlugin, MethodCallHandler {
+  /// The MethodChannel that will the communication between Flutter and native Android
+  ///
+  /// This local reference serves to register the plugin with the Flutter Engine and unregister it
+  /// when the Flutter Engine is detached from the Activity
+  private lateinit var channel : MethodChannel
+
+  override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
+    channel = MethodChannel(flutterPluginBinding.binaryMessenger, "flutter_quick_look")
+    channel.setMethodCallHandler(this)
+  }
+
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
+    if (call.method == "getPlatformVersion") {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
     }
   }
 
-  override fun onMethodCall(call: MethodCall, result: Result) {    
-    result.notImplemented()
+  override fun onDetachedFromEngine(@NonNull binding: FlutterPlugin.FlutterPluginBinding) {
+    channel.setMethodCallHandler(null)
   }
 }
---
+++ example/android/app/src/main/kotlin/com/alva2/flutter/quicklook/example/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.alva2.flutter.quicklook.example
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
